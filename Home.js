f7c9ee003f8206ec02b4fce837bfcaa8d629b2f5
DiogoSilva <diogo.dvs.white@gmail.com>
2022-02-05 11:12:20 -0300

    solucionado problema do JS.

---
+++ Home.js
@@ -0,0 +1,27 @@
+let time = 6000,
+    currentImageIndex = 0,
+    images = document
+                .querySelectorAll(".carrosel img")
+    max = images.length;
+
+function nextImage() {
+
+    images[currentImageIndex].classList.remove("selected")
+
+    currentImageIndex++
+
+    if(currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex]
+        .classList.add("selected")
+}
+
+function start() {
+    setInterval(() => {
+        // troca de image
+        nextImage()
+    }, time)
+}
+
+window.addEventListener("load", start)
