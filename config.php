a2f5176ed3356c7a3ac0652118089b120e319346
DiogoSilva <diogo.dvs.white@gmail.com>
2022-02-07 11:46:10 -0300

    pagina contato finalizada
    falta apenas fazer a conexao do formulario com DB

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+    $dbHost = "Localhost";
+    $dbUsername = "root";
+    $dbpassword ="12345678";
+    $dbname = "Kalebe-advocacia"
+
+    $conexao = new mysqli($dbHost, $dbUsername, $dbpassword, $dbname)
+
+//  if($conexao -> connect_errno)
+//     {
+//        echo "Erro";
+//  }
+//  else
+//  {
+//        echo "Conexao efetuada com sucesso";
+//  }
+
+
+?>
